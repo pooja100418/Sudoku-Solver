8d70cd7da5d7da3771b5036e8638ea863515532f
unknown <poojapuneet100418@gmail.com>
2023-10-18 18:49:05 +0530

    adding files for the project

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * React component for the Footer Section.
+ */
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Developed By Pooja Yadav</p>
+    </footer>
+  )
+}
